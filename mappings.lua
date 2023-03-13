40e4bb283b9292e4ecd268a2739ff9f65be11069
Torfinn Nome <torfinn.nome@gmail.com>
2023-03-13 14:29:10 +0100

    Resize with arrows

--- mappings.lua
+++ mappings.lua
@@ -8,6 +8,35 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    ["<C-Down>"] = false,
+		["<C-Left>"] = false,
+		["<C-Right>"] = false,
+		["<C-Up>"] = false,
+    -- resize with arrows
+		["<Up>"] = {
+			function()
+				require("smart-splits").resize_up(2)
+			end,
+			desc = "Resize split up",
+		},
+		["<Down>"] = {
+			function()
+				require("smart-splits").resize_down(2)
+			end,
+			desc = "Resize split down",
+		},
+		["<Left>"] = {
+			function()
+				require("smart-splits").resize_left(2)
+			end,
+			desc = "Resize split left",
+		},
+		["<Right>"] = {
+			function()
+				require("smart-splits").resize_right(2)
+			end,
+			desc = "Resize split right",
+		},
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
