69f4a4777efa8912c369e87378ec615195214501
Torfinn Nome <torfinn.nome@gmail.com>
2023-03-15 22:27:51 +0100

    Disable auto-formatting

--- init.lua
+++ init.lua
@@ -1,4 +1,13 @@
 return {
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  lsp = {
+    -- customize lsp formatting options
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = false, -- enable or disable format on save globally
+      },
+    },
+  },
 }
