5eec1267f11a3ac71203dfa5bac827a87b92e915
Torfinn Nome <torfin@usit.uio.no>
2023-08-01 14:53:36 +0200

    wildmode: Add bash-style completion

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    wildmode = "longest,list,full", -- bash-like tab completion
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
